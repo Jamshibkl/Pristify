38953317b8402ebbbed65f121e8ea55aedb518b6
Mohammed Jamsheer <jamshibkl3@gmail.com>
2024-01-08 00:23:41 +0530

    Update About.js

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import './About.css'
 function About() {
   return (
+    <>
+    <Header />
     <div>
-      <Header />
-      <h1>About us</h1>
+       
+      <div className="about-us-container">
+      
+    <h2 className="about-us-heading">About Us</h2>
+    <p className="about-us-paragraph">Welcome to our Lightroom Preset Selling Website! We are passionate about photography and believe that high-quality presets can enhance your photos and make your editing process more efficient.</p>
+    
+    <p className="about-us-paragraph">At [Your Company Name], our mission is to provide photographers with top-notch Lightroom presets that cater to various styles and preferences. Whether you're a professional photographer or an enthusiast, we have presets that suit your needs.</p>
+    
+    <p className="about-us-paragraph">Our team of experienced photographers and graphic designers work tirelessly to create presets that not only elevate your photos but also save you time. We understand the importance of having a unique and stunning visual style, and our presets are designed to help you achieve just that.</p>
+    
+    <p className="about-us-paragraph">Feel free to explore our collection of presets and find the perfect ones for your creative projects. If you have any questions or suggestions, don't hesitate to reach out to us. We're here to support you on your photography journey!</p>
+  </div>
+     
     </div>
+    </>
+    
+      
   )
 }
 
