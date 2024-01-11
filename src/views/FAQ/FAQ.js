b6085d2a09711ec201f34ee6290e5d0b1ef83970
jamsheer <jamshibkl3@gmail.com>
2024-01-11 13:41:51 +0530

    content via added

--- src/views/FAQ/FAQ.js
+++ src/views/FAQ/FAQ.js
@@ -24,16 +24,16 @@ function FAQ() {
           You can tweak settings like exposure, 
           contrast, and color balance to suit your preferences.
         </MDBAccordionItem>
-        <MDBAccordionItem collapseId={3} headerTitle="Can I use Lightroom presets on my mobile device?">
+        <MDBAccordionItem collapseId={4} headerTitle="Can I use Lightroom presets on my mobile device?">
           <strong></strong>Yes, you can use Lightroom presets on your mobile device by installing the Adobe Lightroom mobile app. 
           Once installed, 
           you can import and apply presets to your photos just like on the desktop version.
         </MDBAccordionItem>
-        <MDBAccordionItem collapseId={3} headerTitle="Do I need Adobe Lightroom to use these presets?">
+        <MDBAccordionItem collapseId={5} headerTitle="Do I need Adobe Lightroom to use these presets?">
           <strong></strong>Yes, you will need Adobe Lightroom either the desktop or mobile version to use these presets. 
           They are specifically designed for use within the Lightroom software.
         </MDBAccordionItem>
-        <MDBAccordionItem collapseId={3} headerTitle="Can I customize the presets to suit my own style?">
+        <MDBAccordionItem collapseId={6} headerTitle="Can I customize the presets to suit my own style?">
           <strong></strong>Absolutely! While presets provide a great starting point for editing your photos, 
           you can always make adjustments and fine-tune them to match your personal preferences and creative vision.
         </MDBAccordionItem>
